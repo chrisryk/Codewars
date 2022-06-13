d74216d112a996d05f5eacacc0e38c442fd8ceda
Krzysztof Rykowski <102020955+chrisryk@users.noreply.github.com>
2022-06-13 21:23:53 +0200

    Create Multiples of 3 or 5.cs

---
+++ 6 kyu/Multiples of 3 or 5.cs
@@ -0,0 +1,22 @@
+public static class Kata
+{
+  public static int Solution(int value)
+  {
+    if (value <= 0)
+      return 0;
+    
+    int result = 0;
+    
+    for (int i = 1; i < value; i++)
+    {
+      if (i % 3 == 0 || i % 5 == 0)
+        result += i;
+    }
+    return result;
+  }
+}
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+//Additionally, if the number is negative, return 0 (for languages that do have them).
+//Note: If the number is a multiple of both 3 and 5, only count it once.
